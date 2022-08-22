7936c457528bb104e291d45fbc1e3d0bd9d5f939
meyerjrr <jrmeyer339@gmail.com>
2022-08-22 11:39:32 +1000

    added test skip

--- tokenizer_kms_test.go
+++ tokenizer_kms_test.go
@@ -11,6 +11,9 @@ import (
 func TestKMSEncoder(t *testing.T) {
 	_ = godotenv.Load("./.env")
 	key := os.Getenv("AWS_KMS_KEY")
+	if key == "" {
+		t.Skip("AWS_KMS_KEY is not set")
+	}
 	kmsTokenizer, err := NewKMSTokenizer(context.Background(), key)
 	if err != nil {
 		t.Errorf(err.Error())
